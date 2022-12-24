2b399ad40bd474e4bcfeac64a60e6fb27c8e0cfd
Al <hasan-bondi@hotmail.com>
2022-12-24 15:03:43 +0100

    python part

--- soc_lab_python_part/main.py
+++ soc_lab_python_part/main.py
@@ -1,6 +1,8 @@
 import computationModule
 import approximateAdders
 
+#choose approximate adder
+
 #adder = approximateAdders.zero_truncation_adder
 adder = approximateAdders.loa_adder
 computationModule.computation_module(adder, 16)
